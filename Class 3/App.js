b4790852138bfcf6f0ed0144e1c677727223e836
vineetkhemnani <vineetkhemnani09@gmail.com>
2023-07-27 01:43:53 +0530

    minor comments and notes added

--- Class 3/App.js
+++ Class 3/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 // React.createElement creates a React object=> converted to HTML code and put up on DOM
 // React.createElement=> Object=> Rendered on DOM
 // const heading = React.createElement(
@@ -18,8 +18,8 @@ import ReactDOM from "react-dom/client";
 const Title = () => (
   <h1 id="title" key="h2">
     Namaste React
-    </h1>
-);
+  </h1>
+)
 // console.log(heading)
 
 // React Component
@@ -27,23 +27,24 @@ const HeaderComponent1 = () => {
   return (
     <div>
       {Title()}
+      {/* piece of javascript code */}
       <h2>Namaste React from functional component</h2>
       <h2>This is a h2 tag in HeaderComponent1</h2>
     </div>
   )
 }
+// Composing Components
 const HeaderComponent2 = () => {
   return (
     <div>
-      <Title/>
+      <Title />
       <h2>Namaste React from functional component</h2>
       <h2>This is a h2 tag in Header-Component2</h2>
     </div>
   )
 }
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'))
 // render injects it into our DOM into the root
 // react root overwrites whatever is written in it
-root.render(<HeaderComponent2/>)
+root.render(<HeaderComponent2 />)
